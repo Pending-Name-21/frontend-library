92614f8639a5ef24e820a5fde1d278bc4a79a54d
Terán Rocha José Luis <joseluisteran2617@gmail.com>
2024-06-12 18:48:11 -0400

    build(publish): add configuration for publishing to Maven Central via Sonatype

--- bridge/build.gradle.kts
+++ bridge/build.gradle.kts
@@ -1,6 +1,19 @@
+object Meta {
+    const val desc = "This library allows the game developer to write games."
+    const val githubRepo = "Pending-Name-21/frontend-library"
+    const val license = "MIT License"
+    const val licenseUrl = "http://www.opensource.org/licenses/mit-license.php"
+    const val developerId = "teran-joseluis"
+    const val developerName = "José Luis Terán"
+    const val developerOrganization = "Pending-Name-21"
+    const val developerOrganizationUrl = "https://github.com/Pending-Name-21"
+}
+
 plugins {
     `java-library`
     id("com.diffplug.spotless") version "6.25.0"
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -15,6 +28,9 @@ dependencies {
 
 // Apply a specific Java toolchain to ease working on different environments.
 java {
+    withJavadocJar()
+    withSourcesJar()
+
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
@@ -33,5 +49,57 @@ spotless {
   }
 }
 
-version = "0.0.0-alpha.0.2.0"
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = findProperty("group") as String
+            artifactId = findProperty("artifact") as String
+            version = "0.0.0-alpha.0.2.0"
 
+            from(components["java"])
+
+            pom {
+                name.set(rootProject.name)
+                description.set(Meta.desc)
+                url.set("https://github.com/${Meta.githubRepo}")
+
+                licenses {
+                    license {
+                        name.set(Meta.license)
+                        url.set(Meta.licenseUrl)
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set(Meta.developerId)
+                        name.set(Meta.developerName)
+                        organization.set(Meta.developerOrganization)
+                        organizationUrl.set(Meta.developerOrganizationUrl)
+                    }
+                }
+
+                scm {
+                    url.set("https://github.com/${Meta.githubRepo}.git")
+                    connection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+                    developerConnection.set("scm:git:git://github.com/${Meta.githubRepo}.git")
+                }
+
+                issueManagement {
+                    url.set("https://github.com/${Meta.githubRepo}/issues")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    val signingKey = providers.environmentVariable("GPG_SIGNING_KEY")
+    val signingPassphrase = providers.environmentVariable("GPG_SIGNING_PASSPHRASE")
+
+    if (signingKey.isPresent && signingPassphrase.isPresent) {
+        useInMemoryPgpKeys(signingKey.get(), signingPassphrase.get())
+        val extension = extensions.getByName("publishing") as PublishingExtension
+        sign(extension.publications)
+    }
+}
