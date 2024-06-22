1775d33e6a887fe6c0b12043782966653554748e
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-06-22 03:24:56 -0400

    feat: added flatbuffer communication protocol
    - [FlatBuffer](https://flatbuffers.dev/) is an efficient serialization
      library. This library uses a human readable schema to describe the
      communication.
    - KeyboardEvent are produced by the Screen when user input is detected.
    - Updated Receiver to de-serialize using flatbuffer.

--- bridge/src/main/java/com/bridge/ipc/Receiver.java
+++ bridge/src/main/java/com/bridge/ipc/Receiver.java
@@ -1,7 +1,7 @@
 package com.bridge.ipc;
 
 import com.bridge.processinputhandler.AEventBuffer;
-
+import CoffeeTime.InputEvents.KeyboardEvent;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
@@ -9,20 +9,22 @@ import java.util.HashMap;
 
 public class Receiver {
     private HashMap<String, AEventBuffer> buffers;
+    private final ByteBuffer buffer;
+
+    public Receiver() {
+        buffer = ByteBuffer.allocate(1024);
+    }
 
     protected void handleMessage(SocketChannel channel) throws IOException {
-        // TODO: implement logic
-        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.clear();
         int bytesRead = channel.read(buffer);
         if (bytesRead < 0) {
             System.out.print("Got empty message\n");
             return;
         }
 
-        byte[] bytes = new byte[bytesRead];
-        buffer.flip();
-        buffer.get(bytes);
-        String message = new String(bytes);
-        System.out.printf("Got message: %s\n", message);
+        buffer.rewind();
+        KeyboardEvent keyboardEvent = KeyboardEvent.getRootAsKeyboardEvent(buffer);
+        System.out.printf("Got message: %s\n", keyboardEvent.name());
     }
 }
