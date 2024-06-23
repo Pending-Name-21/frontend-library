c0e5148005ac891dbef9c41b9871d9e18e50146d
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-06-22 21:20:09 -0400

    doc: added implementation documentation

--- bridge/src/main/java/com/bridge/ipc/Receiver.java
+++ bridge/src/main/java/com/bridge/ipc/Receiver.java
@@ -1,17 +1,36 @@
 package com.bridge.ipc;
 
 import CoffeeTime.InputEvents.KeyboardEvent;
-
 import java.nio.ByteBuffer;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+/**
+ * The {@code Receiver} class is responsible for handling and processing incoming keyboard events.
+ * It receives messages in the form of a {@link ByteBuffer}, converts them to {@link KeyboardEvent} objects,
+ * and stores them in a thread-safe queue.
+ */
 public class Receiver {
+
+    /**
+     * A thread-safe queue to store keyboard events.
+     */
     private final ConcurrentLinkedQueue<KeyboardEvent> keyboardEvents;
 
+    /**
+     * Constructs a {@code Receiver} with the specified queue to store keyboard events.
+     *
+     * @param keyboardEvents the queue to store the keyboard events
+     */
     public Receiver(ConcurrentLinkedQueue<KeyboardEvent> keyboardEvents) {
         this.keyboardEvents = keyboardEvents;
     }
 
+    /**
+     * Processes the incoming message buffer, converts it to a {@link KeyboardEvent},
+     * and adds it to the queue of keyboard events.
+     *
+     * @param buffer the message buffer containing the keyboard event data
+     */
     public void handleMessage(ByteBuffer buffer) {
         KeyboardEvent keyboardEvent = KeyboardEvent.getRootAsKeyboardEvent(buffer);
         System.out.printf("Got message: %s\n", keyboardEvent.name());
--- bridge/src/test/java/com/bridge/ipc/SocketServerTest.java
+++ bridge/src/test/java/com/bridge/ipc/SocketServerTest.java
@@ -1,16 +1,16 @@
 package com.bridge.ipc;
 
+import java.util.concurrent.ConcurrentLinkedQueue;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import java.util.concurrent.ConcurrentLinkedQueue;
 
 class SocketServerTest {
 
     @Test
     @Disabled
     void startServer() {
-        SocketServer socketServer = new SocketServer(new Receiver(new ConcurrentLinkedQueue<>()), "events-socket.sock");
+        SocketServer socketServer =
+                new SocketServer(new Receiver(new ConcurrentLinkedQueue<>()), "events-socket.sock");
         socketServer.flush();
         socketServer.run();
         System.out.println("Closed socket server");
