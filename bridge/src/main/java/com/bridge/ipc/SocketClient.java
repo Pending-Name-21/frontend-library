ebf8ea4ea3601758998fc5cd91812bb4c0b39220
Santiago C. Saavedra <zrock_git@zohomail.com>
2024-06-23 16:57:46 -0400

    doc: added overall implementation documentation

--- bridge/src/main/java/com/bridge/ipc/SocketClient.java
+++ bridge/src/main/java/com/bridge/ipc/SocketClient.java
@@ -6,9 +6,18 @@ import java.net.UnixDomainSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
 
+/**
+ * This class provides a client-side connection for inter-process communication (IPC)
+ * using Unix domain sockets.
+ */
 public class SocketClient {
     private SocketChannel channel;
 
+    /**
+     * Constructs a new `SocketClient` instance connected to the specified namespace.
+     *
+     * @param namespace The name of the Unix domain socket to connect to.
+     */
     public SocketClient(String namespace) {
         try {
             channel = SocketChannel.open(StandardProtocolFamily.UNIX);
@@ -20,6 +29,13 @@ public class SocketClient {
         }
     }
 
+    /**
+     * Sends the provided byte buffer through the established socket connection.
+     * This method performs a non-blocking write, sending as much data as possible
+     * from the buffer until it is empty.
+     *
+     * @param buffer The byte buffer containing the data to be sent.
+     */
     protected void send(ByteBuffer buffer) {
         while (buffer.hasRemaining()) {
             try {
--- bridge/src/main/java/com/bridge/renderHandler/render/Frame.java
+++ bridge/src/main/java/com/bridge/renderHandler/render/Frame.java
@@ -4,4 +4,9 @@ import com.bridge.renderHandler.sound.Sound;
 import com.bridge.renderHandler.sprite.Sprite;
 import java.util.List;
 
+/**
+ * Represents a rendering frame containing sprites and sounds.
+ * This class is immutable and serves as a data container.
+ *
+ */
 public record Frame(List<Sprite> sprites, List<Sound> sounds) {}
